4314d2be301165c90509fe51a3cf38556a065d7a
sergkol557 <sergkol557@gmail.com>
2017-08-01 14:53:46 +0300

    some fixes

--- pset2/task5/js/script.js
+++ pset2/task5/js/script.js
@@ -1,17 +1,26 @@
 function showYear() {
 	var number = document.getElementById('year').value;
 
+
 	if (number > 150) {
-		alert('your age is much big');
-		document.getElementById('year').value = '0';
+
+		document.getElementById('result').innerText = 'your age is much big';
+		return;
 
 	} else if (number == "") {
-		alert('you must input age');
-		document.getElementById('time').value = '0';
+		
+		document.getElementById('result').innerText = 'you must input age';
+		return;
 
 	} else {
 		var years = ["лет", "год", "года", "года", "года", "лет", "лет", "лет", "лет", "лет"];
+		var result = years[ String(number).slice(-1) ];
 
-		document.getElementById('result').innerText = number + " " + years[ String(number).slice(-1) ];
+		if ( number.toString(number).length > 1 && String(number).slice(-2, -1) == 1) {
+
+			result = "лет";
+		}
+
+		document.getElementById('result').innerText = number + " " + result;
 	}
 }
