d51e15631b73db7ad084ba01e6b4ba3cc1662044
Sk <Sk>
2017-08-07 06:22:33 +0300

    task3 completed

---
+++ pset3/task2/js/script.js
@@ -0,0 +1,40 @@
+var top_show = 100;
+var delay = 1000;
+
+jQuery(document).ready(function () {
+
+    if (jQuery(this).scrollTop() > top_show) $('#top').fadeIn();
+    else jQuery('#top').fadeOut();
+
+    jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() > top_show) $('#top').fadeIn();
+        else jQuery('#top').fadeOut();
+    });
+
+    jQuery('#top').click(function () {
+        jQuery('body,html').animate( {scrollTop: 0}, delay);
+    });
+
+    jQuery('#nav').find('a').click(function () {
+        var id = jQuery(this).attr('data-id');
+        var position_id = jQuery('#'+id).parent().attr('id');
+
+        swapElements(position_id);
+
+        jQuery("html, body").delay(delay).animate({scrollTop: $('#'+id).offset().top }, delay);
+    });
+
+});
+
+function swapElements(position) {
+
+    if (position == 'center') {
+        return;
+    } else {
+        var centerHtml = jQuery('#center').html();
+        jQuery('#center').html(jQuery('#'+position).html());
+        jQuery('#'+position).html(centerHtml);
+    }
+
+}
+
