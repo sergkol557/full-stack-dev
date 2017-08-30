1f268e5842865c13578109b115db5a48340097a8
sergkol557 <sergkol557@gmail.com>
2017-08-30 07:34:12 +0300

    some fixes

--- DraggableSpeechBalloons/js/script.js
+++ DraggableSpeechBalloons/js/script.js
@@ -4,35 +4,56 @@ $(document).ready(function () {
 	$("#img").dblclick(function (e) {
 
 
-		var pos = $(this).offset(); 
-		var relX = e.pageX;
-		var relY = e.pageY;
+			if (e.target.className == 'image-wrapper') {
 
-		$(this).append($('<div>')
-			.addClass('placeddiv')
-			.css({				
-				"left": relX,
-				"top": relY,
-				"width": "100px",
+				$("input").remove();
 
-		})	.draggable({cursor: "move", containment: "#img", scroll: false})
-			.dblclick(function () {
-							$(this).append(
-								$("<input type='text'>")
-									.focus()
-									.val($(this).text())																
-									.keydown(function (e) {
-										if (e.keyCode == 13) {
-											$(this).parent().text($(this).val()).css({"width" : $(this).val().length * 10 + "px"});											
-											$(this).blur().remove();																								
-										}
-										if (e.keyCode == 27) {
-											$(this).blur().remove();											
-										}											
-									})
-								);
-			}));
-	});
+                var pos = $(this).offset();
+                var relX = e.pageX;
+                var relY = e.pageY;
+                var left = $(this).width - 20;
+
+                $(this).append($('<div>')
+                    .addClass('placeddiv')
+                    .css({
+                        "left": relX,
+                        "top": relY,
+                        "width": "100px",
+
+                    }).draggable({
+						cursor: "move",
+						containment: "#img",
+						scroll: false
+                    })
+                    .dblclick(function (e) {
+
+						$("input").remove();
+
+                        $(this).find(".corner").before(
+
+                            $("<input type='text'>")
+                                .val($(this).text())
+                                .keydown(function (e) {
+                                    if (e.keyCode == 13) {
+                                        $(this).parent().text($(this).val()).css({"width": $(this).val().length * 10 + "px"});
+                                        $("input").blur().remove();
+                                    }
+                                    if (e.keyCode == 27) {
+                                        $("input").blur().remove();
+                                    }
+                                })
+                        );
+
+                        $(this).find("input").focus().select();
+                    }).append(
+
+                        $("<div>").addClass("corner").css({
+                            "left" : "60px",
+                            "top" : "20px",
+                        })
+                    )
+				);
+            }});
 
 
 });
