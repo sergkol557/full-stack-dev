0bf01ff0b42c7fbd3c978ebd602df04a1cab781a
sergkol557 <sergkol557@gmail.com>
2017-09-01 18:48:35 +0300

    release baloons

--- DraggableSpeechBalloons/js/script.js
+++ DraggableSpeechBalloons/js/script.js
@@ -1,59 +1,108 @@
+var baloons = 0;
+
 $(document).ready(function () {
+
 	$("#header").draggable();
 
+	$.post("content.php", 
+		{
+			action: "load", 
+		},		
+		function (result) {
+						
+			for ( var id in result) {
+				
+				addBaloon("empty", id, result[id]['left'], result[id]['top'], result[id]['text']);
+			}
+
+	},'json');
+
+
 	$("#img").dblclick(function (e) {
-
-
-			if (e.target.className == 'image-wrapper') {
-
-				$("input").remove();
-
-                var pos = $(this).offset();
-                var relX = e.pageX;
-                var relY = e.pageY;
-                var left = $(this).width - 20;
-
-                $(this).append($('<div>')
-                    .addClass('placeddiv')
-                    .css({
-                        "left": relX,
-                        "top": relY,
-                        "width": "100px",
-
-                    }).draggable({
-						cursor: "move",
-						containment: "#img",
-						scroll: false
-                    })
-                    .dblclick(function (e) {
-
-						$("input").remove();
-
-                        $(this).find(".corner").before(
-
-                            $("<input type='text'>")
-                                .val($(this).text())
-                                .keydown(function (e) {
-                                    if (e.keyCode == 13) {
-                                        $(this).parent().text($(this).val()).css({"width": $(this).val().length * 10 + "px"});
-                                        $("input").blur().remove();
-                                    }
-                                    if (e.keyCode == 27) {
-                                        $("input").blur().remove();
-                                    }
-                                })
-                        );
-
-                        $(this).find("input").focus().select();
-                    }).append(
-
-                        $("<div>").addClass("corner").css({
-                            "left" : "60px",
-                            "top" : "20px",
-                        })
-                    )
-				);
-            }});
+		addBaloon(e);
+});
 
 
 });
+
+function addBaloon (e, id, left, top, text) {
+
+
+	if (e == "empty" || e.target.className == 'image-wrapper') {	
+		
+		baloons++;
+		
+		var pos = document.getElementById('img').getBoundingClientRect();
+		var relX = left ? left + "px" : e.pageX - (pos.left + pageXOffset);
+		var relY = top ? top + "px" : e.pageY - (pos.top + pageYOffset);
+		var res_id = id ? id : 'baloon' + baloons;
+		
+		
+		
+		$("#img").append($('<div>')
+			.addClass('placeddiv')
+			.attr('id', res_id)
+			.text(text)
+			.css({
+				"position": "absolute",
+				"left": relX,
+				"top": relY,
+				}).draggable({
+					cursor: "move",
+					containment: "#img",
+					scroll: false,
+					stop: function() {
+						var current_position = this.getBoundingClientRect();
+							$.post("content.php", 
+							{
+								action: "coord",
+								id: $(this).attr("id"),
+								left: current_position.left,
+								top: current_position.top,
+								});
+							}
+					})
+					.dblclick(function (e) {
+		
+						if (e.target.className.includes('placeddiv')) {							
+		
+							$("input").remove();
+							var text = $(this).text();
+							$(this).text("");
+							var input = document.createElement("input");
+							$(this).append(
+								$(input)
+								.val(text)
+								.keydown(function (e) {
+									if (e.keyCode == 13) {
+										$.post("content.php", 
+										{
+											action: "addtext",
+											id: $(this).parent().attr("id"),
+											msg: $(this).val(),
+										});
+		
+										$(this).parent().text($(this).val())
+										.css({
+											"width": ($(this).val().length + 2) * 8 + "px"
+											});											
+		
+											$(input).blur().remove();
+									}
+		
+									if (e.keyCode == 27) {
+										$(this).parent().text($(this).val())
+										$(input).blur().remove();
+									}
+								})
+							);
+							
+							$(this).find(input).focus().select();
+
+						}
+					})
+		);
+	}
+
+}
+
